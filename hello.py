53cf40e7b73bc989d48e9edefa130e4d2a44be92
skyran1278 <skyran1278@gmail.com>
2018-02-18 19:34:29 +0800

    init

---
+++ hello.py
@@ -0,0 +1,15 @@
+class Man:
+    def __init__(self, name):
+        self.name = name
+        print("Initilized")
+
+    def hello(self):
+        print("Hello " + self.name + "!")
+
+    def goobye(self):
+        print("Good-bye " + self.name + "!")
+
+
+m = Man("David")
+m.hello()
+m.goobye()
