3c61da15803f7a6cf4dbcfd4521b64da106e529d
skyran1278 <skyran1278@gmail.com>
2018-02-28 18:26:11 +0800

    test

--- hello.py
+++ hello.py
@@ -1,15 +1,63 @@
-class Man:
-    def __init__(self, name):
-        self.name = name
-        print("Initilized")
+import numpy as np
+import matplotlib.pyplot as plt
 
-    def hello(self):
-        print("Hello " + self.name + "!")
+x = np.arange(0, 6, 0.1)
+y = np.sin(x)
 
-    def goobye(self):
-        print("Good-bye " + self.name + "!")
+plt.plot(x, y)
+plt.show()
 
 
-m = Man("David")
-m.hello()
-m.goobye()
+# x = np.array([1.0, 2.0, 3.0])
+# print(x)
+# type(x)
+
+# class Man:
+#     def __init__(self, name):
+#         self.name = name
+#         print("Initilized")
+
+#     def hello(self):
+#         print("Hello " + self.name + "!")
+
+#     def goobye(self):
+#         print("Good-bye " + self.name + "!")
+
+
+# m = Man("David")
+# m.hello()
+# m.goobye()
+with expression as target:
+    pass
+
+
+def funcname(self, parameter_list):
+    pass
+
+
+def funcname(parameter_list):
+    pass
+
+
+@staticmethod
+def funcname(parameter_list):
+    pass
+
+
+def funcname(self, parameter_list):
+    raise NotImplementedError
+
+
+class classname(object):
+    pass
+
+
+lambda parameter_list: expression
+
+
+def main():
+    dsjfklds
+
+
+if __name__ == '__main__':
+    main()
