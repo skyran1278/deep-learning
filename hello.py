c8e3e9935a2c2f3dd5920c5827a4d92d325f758d
skyran1278 <skyran1278@gmail.com>
2018-03-11 23:35:42 +0800

    settings

--- hello.py
+++ hello.py
@@ -1,64 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 6, 0.1)
-y = np.sin(x)
-
-plt.plot(x, y)
-plt.show()
-
-
-# x = np.array([1.0, 2.0, 3.0])
-# print(x)
-# type(x)
-
-# class Man:
-#     def __init__(self, name):
-#         self.name = name
-#         print("Initilized")
-
-#     def hello(self):
-#         print("Hello " + self.name + "!")
-
-#     def goobye(self):
-#         print("Good-bye " + self.name + "!")
-# 測試
-for target_list in expression_list:
-    pass
-# m = Man("David")
-# m.hello()
-# m.goobye()
-with expression as target:
-    pass
-
-
-def funcname(self, parameter_list):
-    pass
-
-
-def funcname(parameter_list):
-    pass
-
-
-@staticmethod
-def funcname(parameter_list):
-    pass
-
-
-def funcname(self, parameter_list):
-    raise NotImplementedError
-
-
-class classname(object):
-    pass
-
-
-lambda parameter_list: expression
-
-
-def main():
-    dsjfklds
-
-
-if __name__ == '__main__':
-    main()
+import random
+x = eval(input('請猜1~5的一個號碼'))
+y = random.randint(0, 5)
+if x == y:
+    print('您猜對了，答案正是', x, sep='')
+else:
+    print('您猜錯了喔～其實是', y, sep='')
