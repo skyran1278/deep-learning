0a0a497ba09817b4de9652d58af8215b4dae0db1
skyran1278 <skyran1278@gmail.com>
2018-10-01 23:42:24 +0800

    弄懂 module

--- deep-learning-from-scratch/ch03/mnist_show.py
+++ deep-learning-from-scratch/ch03/mnist_show.py
@@ -1,6 +1,10 @@
 # coding: utf-8
-import sys, os
+import sys
+import os
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+# sys.path.append(os.path.abspath(os.path.join(
+#     os.path.dirname(__file__), os.pardir)))
+
 import numpy as np
 from dataset.mnist import load_mnist
 from PIL import Image
@@ -9,6 +13,7 @@ from PIL import Image
 def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
+
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
